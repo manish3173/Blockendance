0246951ad7330edf6612ed121eb3eda61576952d
Manish <145283318+manish3173@users.noreply.github.com>
2025-01-28 20:03:56 +0530

    Add files via upload

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,11 @@
 # Python module imports
 import datetime as dt
 import hashlib
+import webbrowser
 from flask import Flask, request, render_template, Response
+from threading import Timer
+from datetime import datetime
+import os
 
 # Importing local functions and classes
 from block import *
@@ -9,8 +13,8 @@ from genesis import create_genesis_block
 from newBlock import next_block, add_block
 from getBlock import find_records
 from checkChain import check_integrity
-import smartpy
-from smartpy)functions import *
+from smartpy_functions import *
+
 # Flask declarations
 app = Flask(__name__)
 response = Response()
@@ -34,12 +38,10 @@ def display_blocks(blockchain):
         blocks_info.append(block_info)
     return blocks_info
 
-# Default Landing page of the app
 @app.route('/',  methods=['GET'])
 def index():
     return render_template("index.html")
 
-# Get Form input and decide what is to be done with it
 @app.route('/', methods=['POST'])
 def parse_request():
     if request.form.get("name"):
@@ -48,8 +50,8 @@ def parse_request():
         data.append(request.form.get("name"))
         data.append(str(dt.date.today()))
         return render_template("class.html",
-                                name=request.form.get("name"),
-                                date=dt.date.today())
+                             name=request.form.get("name"),
+                             date=dt.date.today())
 
     elif request.form.get("number"):
         while len(data) > 2:
@@ -57,45 +59,54 @@ def parse_request():
         data.append(request.form.get("course"))
         data.append(request.form.get("year"))
         return render_template("attendance.html",
-                                name=data[0],
-                                course=request.form.get("course"),
-                                year=request.form.get("year"),
-                                number=int(request.form.get("number")))
+                             name=data[0],
+                             course=request.form.get("course"),
+                             year=request.form.get("year"),
+                             number=int(request.form.get("number")))
     elif request.form.get("roll_no1"):
         while len(data) > 4:
             data.pop()
-        return render_template("result.html", result=add_block(request.form, data, blockchain))
-
+        return render_template("result.html", 
+                             result=add_block(request.form, data, blockchain),
+                             datetime=datetime)
     else:
         return "Invalid POST request. This incident has been recorded."
 
-# Show page to get information for fetching records
 @app.route('/view.html',  methods=['GET'])
 def view():
-    return render_template("class.html")
+    return render_template("search_records.html")
 
-# Process form input for fetching records from the blockchain
 @app.route('/view.html',  methods=['POST'])
 def show_records():
-    data = []
-    data = find_records(request.form, blockchain)
-    if data == -1:
-        return "Records not found"
-    return render_template("view.html",
+    try:
+        data = find_records(request.form, blockchain)
+        if data == -1:
+            return render_template("result.html", 
+                                result="No records found for the given criteria",
+                                datetime=datetime)
+        return render_template("view.html",
                             name=request.form.get("name"),
                             course=request.form.get("course"),
                             year=request.form.get("year"),
                             status=data,
                             number=int(request.form.get("number")),
                             date=request.form.get("date"))
+    except Exception as e:
+        return render_template("result.html", 
+                            result=f"Error retrieving records: {str(e)}",
+                            datetime=datetime)
 
-# Show page with result of checking blockchain integrity
 @app.route('/result.html',  methods=['GET'])
 def check():
-    return render_template("result.html", result=check_integrity(blockchain))
+    return render_template("result.html", 
+                         result=check_integrity(blockchain),
+                         datetime=datetime)
 
+def open_browser():
+    webbrowser.open_new('http://localhost:5000/')
 
-
-# Start the flask app when program is executed
 if __name__ == "__main__":
-    app.run()
+    app.debug = True
+    if not os.environ.get('WERKZEUG_RUN_MAIN'):
+        Timer(1.5, open_browser).start()
+    app.run(host='localhost', port=5000)
